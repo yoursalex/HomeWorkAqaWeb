ef7974992b4aab9292a9672f624f81451d68ac2a
yoursalex <sash34@mail.ru>
2019-10-27 23:51:02 +0300

    Tests added

---
+++ src/test/java/CardOrderTests.java
@@ -0,0 +1,64 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardOrderTests {
+
+    @Test
+    @DisplayName("Должен успешно отправлять заявку при валидных данных")
+    void ShouldSubmitRequest() {
+        open("http://localhost:9999/");
+        $("[data-test-id=name] input.input__control").setValue("Иванов Иван");
+        $("[data-test-id=phone] input.input__control").setValue("+79991112233");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $("[data-test-id=order-success]").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    @DisplayName("Должен указывать на ошибку при вводе невалидных фамилии и имени")
+    void ShouldNotSubmitWithWrongName() {
+        open("http://localhost:9999/");
+        $("[data-test-id=name] input.input__control").setValue("Ivanov Ivan");
+        $("[data-test-id=phone] input.input__control").setValue("+79991112233");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $(".input_invalid").shouldHave(Condition.text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    @DisplayName("Должен указывать на ошибку при вводе невалидного номера телефона")
+    void ShouldNotSubmitWithWrongPhone() {
+        open("http://localhost:9999/");
+        $("[data-test-id=name] input.input__control").setValue("Иванов Иван");
+        $("[data-test-id=phone] input.input__control").setValue("+799911122");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $(".input_invalid").shouldHave(Condition.text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    @DisplayName("Должен указывать на ошибку при пустом поле имени")
+    void ShouldNotSubmitWithEmptyName() {
+        open("http://localhost:9999/");
+        $("[data-test-id=phone] input.input__control").setValue("+79991112233");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $(".input_invalid").shouldHave(Condition.text("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    @DisplayName("Должен указывать на ошибку при пустом поле номера телефона")
+    void ShouldNotSubmitWithEmptyPhone() {
+        open("http://localhost:9999/");
+        $("[data-test-id=name] input.input__control").setValue("Иванов Иван");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $(".input_invalid").shouldHave(Condition.text("Поле обязательно для заполнения"));
+    }
+
+}
